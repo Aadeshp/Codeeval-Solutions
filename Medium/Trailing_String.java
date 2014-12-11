45a1255b5ec1c41b490193e5e5b58be361c00bdf
Aadesh Patel <aadeshpatel@Aadeshs-MacBook-Pro.local>
2014-12-10 20:54:12 -0500

    Got rid of unnecessary sample code

--- Medium/Trailing_String.java
+++ Medium/Trailing_String.java
@@ -1,18 +1,3 @@
-/* Sample code to read in test cases:
-import java.io.*;
-public class Main {
-    public static void main (String[] args) throws IOException {
-        File file = new File(args[0]);
-        BufferedReader buffer = new BufferedReader(new FileReader(file));
-        String line;
-        while ((line = buffer.readLine()) != null) {
-            line = line.trim();
-            // Process line of input Here
-        }
-    }
-}
-*/
-
 import java.io.*;
 import java.util.*;
 
